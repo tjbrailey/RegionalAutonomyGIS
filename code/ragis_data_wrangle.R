772293c0eb445762015005543340fcfe1596f89f
tjbrailey <tbrailey@ucsd.edu>
2019-11-15 10:53:14 -0800

    big push

---
+++ code/ragis_data_wrangle.R
@@ -0,0 +1,56 @@
+# Regional Autonomy GIS data wrangling
+
+# Setup
+rm(list=ls())
+
+library(magrittr)
+library(ggplot2)
+
+# Load data
+epr <- rio::import(paste0(here::here(), "/data/EPR-2018.1.1.csv"))
+geo_epr <- rio::import(paste0(here::here(), "/data/GeoEPR-2018.1.1.csv"))
+mali_conflict <- rio::import(paste0(here::here(), "/data/1997-01-01-2003-12-31-Mali.csv"))
+
+# Subset data
+group_t <- epr %>%
+  dplyr::filter(reg_aut == "TRUE") %>%
+  dplyr::distinct(group) %>%
+  dplyr::pull()
+
+group_f <- epr %>%
+  dplyr::filter(reg_aut == "FALSE") %>%
+  dplyr::distinct(group) %>%
+  dplyr::pull()
+
+group_t <- dplyr::as_tibble(group_t)
+group_f <- dplyr::as_tibble(group_f)
+
+group_aut <- group_t %>%
+  dplyr::filter(value %in% unique(group_f$value))
+
+epr_sub <- epr %>%
+  dplyr::filter(group %in% unique(group_aut$value))
+
+geo_epr_sub <- geo_epr %>%
+  dplyr::filter(group %in% unique(group_aut$value)) %>%
+  dplyr::select(-from, -to)
+
+epr_join <- dplyr::left_join(epr_sub, geo_epr_sub)
+unique(epr_join$statename)
+
+af_sub <- c("Mali", "Nigeria", "Chad", "Congo, DRC", "Comoros", "Sudan")
+
+epr_join_af <- epr_join %>%
+  dplyr::filter(statename %in% c(af_sub)) %>% 
+  dplyr::group_by(statename, from, to, group) %>%
+  dplyr::summarise_all(dplyr::funs(dplyr::first(na.omit(.)))) %>% 
+  dplyr::filter(status != "SELF-EXCLUSION" &
+                  !is.na(reg_aut) &
+                  type != "Urban") %>%
+  dplyr::group_by(statename, group) %>%
+  tidyr::complete(group, from = min(from):2019) %>%
+  tidyr::fill(gwid, groupid, gwgroupid, umbrella, size, status, reg_aut, sqkm, type, the_geom) %>%
+  dplyr::select(-to) %>%
+  dplyr::rename(year = from)
+
+#rm(epr, epr_join, epr_sub, geo_epr, geo_epr_sub, group_aut, group_f, group_t, af_sub)
